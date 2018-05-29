d6d1439c5289b5559080f94b9dc25b4c4f401a25
奔波儿灞 <xuanbo.wang01@hand-china.com>
2018-05-29 11:10:25 +0800

    DataFrame

---
+++ src/main/java/com/example/spark/sql/DataFrameApp.java
@@ -0,0 +1,47 @@
+package com.example.spark.sql;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+/**
+ * DataFrames
+ *
+ * bash:
+ * bin/spark-submit --class "com.example.spark.sql.DataFrameApp" spark-example-1.0-SNAPSHOT.jar
+ *
+ * @author xuan
+ * @since 1.0.0
+ */
+public class DataFrameApp {
+
+    public static void main(String[] args) {
+        // The entry point into all functionality in Spark is the SparkSession class
+        SparkSession spark = SparkSession
+                .builder()
+                .appName(DataFrameApp.class.getSimpleName())
+                .getOrCreate();
+
+        // creates a DataFrame based on the content of a JSON file
+        Dataset<Row> df = spark.read().json("file:///usr/local/spark/people.json");
+
+        // Displays the content of the DataFrame to stdout
+        df.show();
+
+        // Select only the "name" column
+        df.select("name").show();
+
+        // Select everybody, but increment the age by 1
+        df.select(df.col("name"), df.col("age").plus(1)).show();
+
+        // Select people older than 21
+        df.filter(df.col("age").gt(21)).show();
+
+        // save
+        df.javaRDD().saveAsTextFile("file:///usr/local/spark/people.txt");
+
+        // close
+        spark.close();
+    }
+
+}
