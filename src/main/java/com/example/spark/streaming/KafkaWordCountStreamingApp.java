29ff2759150a7791edf115cd1ee1a7a8cad01639
奔波儿灞 <xuanbo.wang01@hand-china.com>
2018-06-05 20:36:09 +0800

    spark streaming kafka统计单词

---
+++ src/main/java/com/example/spark/streaming/KafkaWordCountStreamingApp.java
@@ -0,0 +1,72 @@
+package com.example.spark.streaming;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scala.Tuple2;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 从kafka中获取流，每10秒统计一次
+ *
+ * bash:
+ * bin/spark-submit --class "com.example.spark.streaming.KafkaWordCountStreamingApp" spark-example-1.0-SNAPSHOT.jar
+ *
+ * @author xuan
+ * @since 1.0.0
+ */
+public class KafkaWordCountStreamingApp {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaWordCountStreamingApp.class);
+
+    public static void main(String[] args) throws InterruptedException {
+        // Create a StreamingContext
+        SparkConf conf = new SparkConf().setAppName(KafkaWordCountStreamingApp.class.getSimpleName());
+        JavaStreamingContext ssc = new JavaStreamingContext(conf, Durations.seconds(10));
+
+        Map<String, Object> kafkaParams = new HashMap<>();
+        kafkaParams.put("bootstrap.servers", "47.104.131.255:9092");
+        kafkaParams.put("key.deserializer", StringDeserializer.class);
+        kafkaParams.put("value.deserializer", StringDeserializer.class);
+        kafkaParams.put("group.id", KafkaWordCountStreamingApp.class.getName());
+        kafkaParams.put("auto.offset.reset", "latest");
+        kafkaParams.put("enable.auto.commit", false);
+
+        Collection<String> topics = Collections.singletonList("test-kafka");
+
+        JavaInputDStream<ConsumerRecord<String, String>> stream =
+                KafkaUtils.createDirectStream(
+                        ssc,
+                        LocationStrategies.PreferConsistent(),
+                        ConsumerStrategies.<String, String>Subscribe(topics, kafkaParams)
+                );
+
+        stream.map(ConsumerRecord::value)
+                .mapToPair(word -> new Tuple2<>(word, 1))
+                .reduceByKey((a, b) -> a + b)
+                .map(tuple -> tuple._1 + ": " + tuple._2)
+                // 存储，这里直接打印了
+                .foreachRDD(rdd -> {
+                    LOG.info("=============================");
+                    rdd.foreachPartition(partitionRecords -> partitionRecords.forEachRemaining(data -> LOG.info("data: {}", data)));
+                    LOG.info("=============================");
+                });
+
+        ssc.start();
+
+        ssc.awaitTermination();
+    }
+
+}
---
+++ src/main/java/com/example/spark/streaming/MessageProducer.java
@@ -0,0 +1,44 @@
+package com.example.spark.streaming;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+/**
+ * 消息生产者
+ *
+ * @author xuan
+ * @since 1.0.0
+ */
+public class MessageProducer {
+
+    public static void main(String[] args) {
+        // 配置
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "47.104.131.255:9092");
+        props.put("acks", "all");
+        props.put("retries", 1);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        // Producer
+        Producer<String, String> producer = new KafkaProducer<>(props);
+
+        // Send
+        producer.send(new ProducerRecord<>("test-kafka", "hello", "hello"));
+        producer.send(new ProducerRecord<>("test-kafka", "hello", "hello"));
+        producer.send(new ProducerRecord<>("test-kafka", "world", "world"));
+        producer.send(new ProducerRecord<>("test-kafka", "world", "world"));
+        producer.send(new ProducerRecord<>("test-kafka", "spring", "spring"));
+        producer.send(new ProducerRecord<>("test-kafka", "spring", "spring"));
+
+        // Close
+        producer.close();
+    }
+
+}
