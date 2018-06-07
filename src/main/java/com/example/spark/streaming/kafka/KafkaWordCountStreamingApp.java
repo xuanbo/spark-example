a5b89fe8e231ac0dcae439b47a884ebfce58dd8e
奔波儿灞 <xuanbo.wang01@hand-china.com>
2018-06-07 12:13:18 +0800

    消费

--- src/main/java/com/example/spark/streaming/kafka/KafkaWordCountStreamingApp.java
+++ src/main/java/com/example/spark/streaming/kafka/KafkaWordCountStreamingApp.java
@@ -44,7 +44,13 @@ public class KafkaWordCountStreamingApp {
         kafkaParams.put("key.deserializer", StringDeserializer.class);
         kafkaParams.put("value.deserializer", StringDeserializer.class);
         kafkaParams.put("group.id", KafkaWordCountStreamingApp.class.getName());
-        kafkaParams.put("auto.offset.reset", "latest");
+        // earliest
+        // 当各分区下有已提交的offset时，从提交的offset开始消费；无提交的offset时，从头开始消费
+        // latest
+        // 当各分区下有已提交的offset时，从提交的offset开始消费；无提交的offset时，消费新产生的该分区下的数据
+        // none
+        // topic各分区都存在已提交的offset时，从offset后开始消费；只要有一个分区不存在已提交的offset，则抛出异常
+        kafkaParams.put("auto.offset.reset", "earliest");
         kafkaParams.put("enable.auto.commit", false);
 
         Collection<String> topics = Collections.singletonList("test-kafka");
